f6fefb777805cb32074769adec09e673a1bf5c58
Rafael Dias <rafaelldiias100@gmail.com>
2022-12-08 23:22:25 -0300

    feat: adicionando os principais componentes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Form from "./src/components/Form";
+import Title from "./src/components/Title";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>MeuIMC!</Text>
-      <StatusBar style="auto" />
+      <Title />
+      <Form />
     </View>
   );
 }
