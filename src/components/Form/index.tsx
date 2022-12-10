31589921e78befe15df0bffd99bb4d063505e033
Rafael Dias <rafaelldiias100@gmail.com>
2022-12-10 17:38:56 -0300

    feat: lista de imcs

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -7,9 +7,15 @@ import {
   View,
   Vibration,
   TouchableOpacity,
+  FlatList,
 } from "react-native";
 import ResultIMC from "./ResultIMC";
 import styles from "./styles";
+
+interface ImcList {
+  id: number;
+  imc: string;
+}
 
 export default function Form() {
   const [height, setHeight] = useState("");
@@ -17,11 +23,14 @@ export default function Form() {
   const [textButton, setTextButton] = useState("Calcular IMC");
   const [imc, setImc] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [imcList, setImcList] = useState<ImcList[]>([]);
 
   function imcCalculator() {
     const formatHeight = Number(height.replace(",", "."));
     const formatWeight = Number(weight.replace(",", "."));
-    return setImc((formatWeight / (formatHeight * formatHeight)).toFixed(2));
+    const imc = (formatWeight / (formatHeight * formatHeight)).toFixed(2);
+    setImc(imc);
+    setImcList((imcList) => [...imcList, { id: new Date().getTime(), imc }]);
   }
 
   function validationImc() {
@@ -74,6 +83,19 @@ export default function Form() {
           </TouchableOpacity>
         </View>
       )}
+      <FlatList
+        data={imcList}
+        style={styles.listImcs}
+        keyExtractor={(item) => {
+          return item.id.toString();
+        }}
+        renderItem={({ item }) => (
+          <Text style={styles.resultImcItem}>
+            <Text style={styles.textResultItemList}>Resultado IMC = </Text>
+            {item.imc}
+          </Text>
+        )}
+      />
     </View>
   );
 }
--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -51,6 +51,19 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "50%",
   },
+  listImcs: {
+    marginTop: 20,
+  },
+  resultImcItem: {
+    fontSize: 24,
+    color: "blue",
+    height: 50,
+    width: "100%",
+    paddingRight: 20,
+  },
+  textResultItemList: {
+    fontSize: 16,
+  },
 });
 
 export default styles;
